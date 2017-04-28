f9ebfb1995b01954254571bff897c118293db2af
Jay Rodgers <headmelted@gmail.com>
2017-04-28 13:23:20 +0000

    Adding tagged commit

--- docs/installers/apt.sh
+++ docs/installers/apt.sh
@@ -1,15 +1,43 @@
 #!/bin/sh
 
-echo "Installing bintray GPG key..."
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61;
-echo "Done!"
+echo "Detecting architecture...";
+MACHINE_MTYPE="$(uname -m)";
+ARCH="${MACHINE_MTYPE}";
+REPOSITORY_NAME="Microsoft";
 
-echo "Installing headmelted GPG key..."
-wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=headmelted | apt-key add -;
-echo "Done!"
+if [ "$ARCH" = "armv7l" ]; then ARCH="armhf"; fi;
+if [ "$ARCH" = "armhf" ]; then REPOSITORY_NAME="headmelted"; fi;
 
-echo "Installing headmelted repository...";
-echo "deb https://dl.bintray.com/headmelted/deb-code-oss wheezy main" > /etc/apt/sources.list.d/headmelted.list;
+echo "Architecture detected as ${ARCH}...";
+
+if [ "${REPOSITORY_NAME}" = "headmelted" ]; then
+
+  echo "Installing bintray GPG key...";
+  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61;
+  echo "Done!";
+
+  echo "Installing headmelted GPG key...";
+  wget -qO - https://bintray.com/user/downloadSubjectPublicKey?username=headmelted | apt-key add -;
+  echo "Done!";
+  
+else
+
+  echo "Retrieving Microsoft GPG key...";
+  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
+  
+  echo "Installing Microsoft GPG key...";
+  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg;
+  
+fi;
+
+echo "Installing ${REPOSITORY_NAME} repository...";
+
+if [ "${REPOSITORY_NAME}" = "headmelted" ]; then
+  echo "deb [arch=armhf,arm64] https://dl.bintray.com/headmelted/deb-code-oss wheezy main" > /etc/apt/sources.list.d/headmelted.list;
+else
+  echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list;
+fi;
+
 if [ $? -eq 0 ]; then
   echo "Repository install complete.";
 else
@@ -22,7 +50,16 @@ apt-get update;
 echo "Done!"
 
 echo "Installing Visual Studio Code...";
-apt-get install -y code-oss;
+
+CODE_EXECUTABLE_NAME="";
+if [ "${REPOSITORY_NAME}" = "headmelted" ]; then
+  CODE_EXECUTABLE_NAME="code-oss";
+else
+  CODE_EXECUTABLE_NAME="code-insiders";
+fi;
+
+apt-get install -y ${CODE_EXECUTABLE_NAME};
+
 if [ $? -eq 0 ]; then
   echo "Visual Studio Code install complete.";
 else
@@ -52,7 +89,7 @@ echo "
 
 Installation complete!
 
-You can start code at any time by calling \"code-oss\" within a terminal.
+You can start code at any time by calling \"${CODE_EXECUTABLE_NAME}\" within a terminal.
 
 A shortcut should also now be available in your desktop menus (depending on your distribution).
 
