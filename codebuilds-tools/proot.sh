18147410568040e85bd53cc2b80f42ea187e7c41
Jay Rodgers <headmelted@gmail.com>
2017-05-05 14:48:19 +0000

    Adding rpm testing (commented out until rpmbuild issues resolved)

--- codebuilds-tools/proot.sh
+++ codebuilds-tools/proot.sh
@@ -1,11 +1,6 @@
-echo "Installing xvfb...";
-sudo apt-get install -y xvfb;
-echo "Retrieving deb package...";
-DEB_PACKAGE=${/deb/*.deb};
-echo "Using dpkg to install ${DEB_PACKAGE}...";
-sudo dpkg -i ${DEB_PACKAGE};
-#echo "Installing missing dependencies for ${DEB_PACKAGE}...";
-#sudo apt-get -f install;
+echo "Installing pre-requisites...";
+sudo apt-get install -y xvfb yum;
+
 echo "Inserting custom xvfb into /etc/init.d...";
 sudo mv -f /codebuilds-tools/xvfb /etc/init.d/xvfb;
 echo "Starting xvfb...";
@@ -16,7 +11,32 @@ echo "Exporting display :99...";
 DISPLAY=:99;
 echo "Launcing dbus...";
 dbus-launch;
+
+# Code to install, test and then remove debian package.
+echo "Retrieving deb package...";
+DEB_PACKAGE=${/deb/*.deb};
+echo "Using dpkg to install ${DEB_PACKAGE}...";
+sudo dpkg -i ${DEB_PACKAGE};
+#echo "Installing missing dependencies for ${DEB_PACKAGE}...";
+#sudo apt-get -f install;
 echo "Listing contents of proot \/...";
 ls /;
 echo "Running tests...";
 /scripts/test.sh;
+echo "Removing deb package...";
+dpkg --remove code-oss;
+
+# # Code to install, test and then remove rpm package.
+# echo "Retrieving rpm package...";
+# DEB_PACKAGE=${/rpm/*.rpm};
+# echo "Using yum to install ${RPM_PACKAGE}...";
+# sudo dpkg -i ${DEB_PACKAGE};
+# #echo "Installing missing dependencies for ${DEB_PACKAGE}...";
+# #sudo apt-get -f install;
+# echo "Listing contents of proot \/...";
+# ls /;
+# echo "Running tests...";
+# /scripts/test.sh;
+# echo "Removing rpm package...";
+# yum remove code-oss;
+
