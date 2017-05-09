4065f2462195fe572e76c9521a3ff53143d4284c
Jay Rodgers <headmelted@gmail.com>
2017-05-09 17:37:08 +0000

    Copying working directory into docker

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -4,5 +4,5 @@ DOCKER_IMAGE=debian:latest;
 docker pull ${DOCKER_IMAGE};
 docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
-docker cp ${TRAVIS_BUILD_DIR}/codebuilds ${DOCKER_IMAGE}:/codebuilds;
+docker cp ${TRAVIS_BUILD_DIR} ${DOCKER_IMAGE}:/;
 docker run -i -t ${DOCKER_IMAGE} ls /codebuilds;
