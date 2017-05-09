94578b7a181d57c08c5209fd75979f2eff6c8bed
Jay Rodgers <headmelted@gmail.com>
2017-05-09 18:02:21 +0000

    Making directory execute as privileged

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -4,4 +4,4 @@ DOCKER_IMAGE=debian:latest;
 docker pull ${DOCKER_IMAGE};
 docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
-docker run -i -t ${DOCKER_IMAGE} -v ${TRAVIS_BUILD_DIR}:/codebuilds ls /codebuilds;
+docker run -p ${DOCKER_IMAGE} -v ${TRAVIS_BUILD_DIR}:/codebuilds ls /codebuilds;
