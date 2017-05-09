a1d8e457929661e5f466821ced43eab8b8fedde1
Jay Rodgers <headmelted@gmail.com>
2017-05-09 17:57:58 +0000

    Bind mounting Travis directory

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -4,5 +4,4 @@ DOCKER_IMAGE=debian:latest;
 docker pull ${DOCKER_IMAGE};
 docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
-docker cp ${TRAVIS_BUILD_DIR} 3e83c23dba6a;
-docker run -i -t ${DOCKER_IMAGE} ls /codebuilds;
+docker run -i -t ${DOCKER_IMAGE} -v ${TRAVIS_BUILD_DIR}:/codebuilds ls /codebuilds;
