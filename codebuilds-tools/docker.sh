289109703f9ac62ac24110178c303389c6bc1995
Jay Rodgers <headmelted@gmail.com>
2017-05-09 19:01:25 +0000

    and again

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -4,4 +4,4 @@ DOCKER_IMAGE=debian:latest;
 docker pull ${DOCKER_IMAGE};
 docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
-docker run -v ${TRAVIS_BUILD_DIR} -t ${DOCKER_IMAGE} /bin/bash -c "ls /home";
+docker run -v ${TRAVIS_BUILD_DIR} -t ${DOCKER_IMAGE} /bin/bash -c "ls ${TRAVIS_BUILD_DIR}";
