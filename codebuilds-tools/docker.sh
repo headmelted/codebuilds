cce6caf0fad5b92ebdf7f587b510033fe408a90a
Jay Rodgers <headmelted@gmail.com>
2017-05-09 21:08:01 +0000

    Direct build with dependency resolution

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -6,4 +6,4 @@ docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
 echo "Binding [${TRAVIS_BUILD_DIR}]...";
 cd ..;
-docker run -v codebuilds:/codebuilds -i -t ${DOCKER_IMAGE} /bin/bash -c "ls /codebuilds";
+DOCKER_OUTPUT=$(docker run -v codebuilds:/codebuilds -i -t ${DOCKER_IMAGE} /bin/bash -c "ls /codebuilds");
