d02a3cf050984891710a803f647250eb59563118
Jay Rodgers <headmelted@gmail.com>
2017-05-09 17:52:43 +0000

    Removing root slash

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -4,5 +4,5 @@ DOCKER_IMAGE=debian:latest;
 docker pull ${DOCKER_IMAGE};
 docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
-docker cp ${TRAVIS_BUILD_DIR} 3e83c23dba6a:/;
+docker cp ${TRAVIS_BUILD_DIR} 3e83c23dba6a;
 docker run -i -t ${DOCKER_IMAGE} ls /codebuilds;
