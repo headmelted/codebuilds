677ab25d6966be409f136e1c91d8af928bdb230a
Jay Rodgers <headmelted@gmail.com>
2017-05-09 15:13:05 +0000

    Pointing to docker wrapper script

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker pull debian/sid;
-docker run -d -v $(pwd)/build.sh:/build.sh -p 127.0.0.1:80:4567 debian/sid /bin/bash -c "/build.sh;"
+DOCKER_IMAGE=debian/sid;
+docker pull ${DOCKER_IMAGE};
+docker run -d -v $(pwd)/build.sh:/build.sh -p 127.0.0.1:80:4567 ${DOCKER_IMAGE} /bin/bash -c "/build.sh;"
