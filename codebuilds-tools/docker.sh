2c599a9b395980fafdd3d3442e4d0c6d06bc9726
Jay Rodgers <headmelted@gmail.com>
2017-05-09 17:14:13 +0000

    Wrapping build script passed to docker

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -3,5 +3,5 @@
 DOCKER_IMAGE=debian;
 docker pull ${DOCKER_IMAGE};
 docker ps;
-docker run -d -v ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh:/build.sh -p 127.0.0.1:80:4567 ${DOCKER_IMAGE} /bin/bash -c "/build.sh";
+docker run -d -v ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh:/build.sh -p 127.0.0.1:80:4567 ${DOCKER_IMAGE} "/bin/bash -c /build.sh";
 
