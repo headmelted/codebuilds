360a74cc73368ce0154a9027903be23e75e270a8
Jay Rodgers <headmelted@gmail.com>
2017-05-09 19:12:12 +0000

    Adding echo for travis binding

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -4,4 +4,5 @@ DOCKER_IMAGE=debian:latest;
 docker pull ${DOCKER_IMAGE};
 docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
+echo "Binding [${TRAVIS_BUILD_DIR}]...";
 docker run -v ${TRAVIS_BUILD_DIR} -t ${DOCKER_IMAGE} /bin/bash -c "ls ${TRAVIS_BUILD_DIR}";
