a62e11c0ec1a68ced2863d50e6e25ca71175550e
Jay Rodgers <headmelted@gmail.com>
2017-05-09 16:52:38 +0000

    Updating build script reference

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -2,4 +2,4 @@
 
 DOCKER_IMAGE=debian;
 docker pull ${DOCKER_IMAGE};
-docker run -d -v $(pwd)/build.sh:/build.sh -p 127.0.0.1:80:4567 ${DOCKER_IMAGE} /bin/bash -c "${TRAVIS_BUILD_DIR}/build.sh;"
+docker run -d -v $(pwd)/build.sh:/build.sh -p 127.0.0.1:80:4567 ${DOCKER_IMAGE} /bin/bash -c "${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh"
