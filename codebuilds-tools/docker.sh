4efbb2126def902acf06d0fae60516a0c5cca07e
Jay Rodgers <headmelted@gmail.com>
2017-05-09 17:24:01 +0000

    Making build.sh executable

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -2,5 +2,6 @@
 
 DOCKER_IMAGE=debian;
 docker pull ${DOCKER_IMAGE};
+chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
 docker run -d -v ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh:/build.sh -p 127.0.0.1:80:4567 ${DOCKER_IMAGE} "/build.sh";
 
