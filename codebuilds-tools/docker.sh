13024616b9cdf23c890427713beec6610af7a7dd
Jay Rodgers <headmelted@gmail.com>
2017-05-09 18:22:43 +0000

    Referencing latest debian from docker

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -4,4 +4,4 @@ DOCKER_IMAGE=debian;
 docker pull ${DOCKER_IMAGE}:latest;
 docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
-docker run -v ${TRAVIS_BUILD_DIR} -p ${DOCKER_IMAGE} echo "yo";
+docker run -v ${TRAVIS_BUILD_DIR} -d ${DOCKER_IMAGE} echo "yo";
