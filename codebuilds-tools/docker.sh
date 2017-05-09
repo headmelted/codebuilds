2da1fdd07fea8fb95b2264392aae59d6bede3f87
Jay Rodgers <headmelted@gmail.com>
2017-05-09 18:08:34 +0000

    Correcting arguments

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -4,4 +4,4 @@ DOCKER_IMAGE=debian:latest;
 docker pull ${DOCKER_IMAGE};
 docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
-docker run -p ${DOCKER_IMAGE} -v ${TRAVIS_BUILD_DIR}:/codebuilds ls /codebuilds;
+docker run -p -v ${TRAVIS_BUILD_DIR}:/codebuilds ${DOCKER_IMAGE} ls /codebuilds;
