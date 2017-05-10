f4db736a0add03ebee631c8d333321b9d097bc33
Jay Rodgers <headmelted@gmail.com>
2017-05-10 09:16:05 +0000

    Resetting binding

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -8,4 +8,4 @@ echo "Exporting environment variables...";
 echo $(printenv) > env.list;
 
 echo "Binding workspace...";
-docker run -it -v /worspace:/workspace --env-file ./env.list ${DOCKER_IMAGE} /bin/bash -c /workspace/codebuilds-tools/build.sh;
+docker run -it -v ${TRAVIS_BUILD_DIR}:/workspace --env-file ./env.list ${DOCKER_IMAGE} /bin/bash -c /workspace/codebuilds-tools/build.sh;
