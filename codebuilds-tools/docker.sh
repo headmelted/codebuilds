e2311602078994c2b4bdc4a9753c2629ff324f2f
Jay Rodgers <headmelted@gmail.com>
2017-05-10 04:43:32 +0000

    Build test against latest

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -5,5 +5,5 @@ docker pull ${DOCKER_IMAGE};
 docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
 echo "Binding [${TRAVIS_BUILD_DIR}]...";
-cd ..;
-DOCKER_OUTPUT=$(docker run -v codebuilds:/codebuilds -i -t ${DOCKER_IMAGE} /bin/bash -c "ls /codebuilds");
+cd ../..;
+docker run -v codebuilds:/codebuilds -i -t ${DOCKER_IMAGE} /bin/bash -c "ls /codebuilds";
