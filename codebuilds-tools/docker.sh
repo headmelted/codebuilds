86721fba96b87fda329449676fb34809d1505961
Jay Rodgers <headmelted@gmail.com>
2017-05-10 09:19:41 +0000

    Making build script executable

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -7,5 +7,8 @@ docker images;
 echo "Exporting environment variables...";
 echo $(printenv) > env.list;
 
+echo "Making build script executable...";
+chmod +x ./build.sh;
+
 echo "Binding workspace...";
 docker run -it -v ${TRAVIS_BUILD_DIR}:/workspace --env-file ./env.list ${DOCKER_IMAGE} /bin/bash -c /workspace/codebuilds-tools/build.sh;
