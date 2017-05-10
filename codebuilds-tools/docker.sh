0ad827c209e48e4dd5d3a35b8b89425582e8a70a
Jay Rodgers <headmelted@gmail.com>
2017-05-10 08:35:24 +0000

    Removing cd typo from workspace

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -9,4 +9,4 @@ echo "Exporting environment variables...";
 echo $(printenv) > env.list;
 
 echo "Binding workspace...";
-docker run -it -v /worspace:/workspace --env-file ./env.list ${DOCKER_IMAGE} cd /bin/bash -c /workspace/codebuilds-tools/build.sh;
+docker run -it -v /worspace:/workspace --env-file ./env.list ${DOCKER_IMAGE} /bin/bash -c /workspace/codebuilds-tools/build.sh;
