dc6bb8311dbebc1680b59d6f94f4316718e78ff5
Jay Rodgers <headmelted@gmail.com>
2017-05-10 08:11:46 +0000

    Mounting at /workspace

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -5,5 +5,4 @@ docker pull ${DOCKER_IMAGE};
 docker images;
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
 echo "Binding [${TRAVIS_BUILD_DIR}]...";
-cd ../..;
-docker run -v ${TRAVIS_BUILD_DIR} -i -t ${DOCKER_IMAGE} /bin/bash -c ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
+docker run -it -v ${TRAVIS_BUILD_DIR}:/workspace ${DOCKER_IMAGE} /bin/bash -c /workspace/codebuilds-tools/build.sh;
