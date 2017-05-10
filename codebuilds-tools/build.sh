b08777a5eb900f4ba3d190e65e3b6c7c415f9887
Jay Rodgers <headmelted@gmail.com>
2017-05-10 14:11:01 +0000

    Explicitly enabling QEMU

--- codebuilds-tools/build.sh
+++ codebuilds-tools/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CXX="${GPP_COMPILER}" CC="${GCC_COMPILER}";
+export CXX="${GPP_COMPILER}" CC="${GCC_COMPILER}" DEBIAN_FRONTEND="noninteractive";
 
 if [[ "${CROSS_TOOLCHAIN}" == "true" ]]; then
 
@@ -17,6 +17,9 @@ if [[ "${CROSS_TOOLCHAIN}" == "true" ]]; then
 
   echo "Installing apt pre-requisites...";
   apt-get install -y python curl xvfb git libwww-perl libexpat1 libxml-libxml-perl libxml-sax-expat-perl gcc-${GNU_TRIPLET} g++-${GNU_TRIPLET} crossbuild-essential-${ARCH} libstdc++6-${ARCH}-cross dpkg-dev perl libconfig-inifiles-perl libxml-simple-perl liblocale-gettext-perl libgcc1 libgcc1:${ARCH} libdpkg-perl libconfig-auto-perl libdebian-dpkgcross-perl ucf debconf dpkg-cross libdbus-1-3:${ARCH} libffi6:${ARCH} libpcre3:${ARCH} libselinux1:${ARCH} libp11-kit0:${ARCH} libcomerr2:${ARCH} libk5crypto3:${ARCH} libkrb5-3:${ARCH} libpango-1.0-0:${ARCH} libpangocairo-1.0-0:${ARCH} libpangoft2-1.0-0:${ARCH} libxcursor1:${ARCH} libxfixes3:${ARCH} libglib2.0-0:${ARCH} libfreetype6:${ARCH} libavahi-client3:${ARCH} libgssapi-krb5-2:${ARCH} libexpat1:${ARCH} libjpeg8:${ARCH} libpng-dev libpng-dev:${ARCH} libtiff5:${ARCH} fontconfig-config libgdk-pixbuf2.0-common libgdk-pixbuf2.0-0:${ARCH} libfontconfig1:${ARCH} libcups2:${ARCH} libcairo2:${ARCH} libc6 libc6:${ARCH} libc6-dev:${ARCH} libatk1.0-0:${ARCH} libgtk2.0-0:${ARCH} libx11-dev:${ARCH} libxkbfile-dev:${ARCH} libx11-xcb-dev libx11-xcb-dev:${ARCH} libxtst6 libxtst6:${ARCH} libxss-dev libxss-dev:${ARCH} libgconf-2-4:${ARCH} libasound2:${ARCH} qemu binfmt-support qemu-user-static rpm;
+
+  echo "Enabling ${PACKAGE_ARCH} emulator...";
+  update-binfmts --enable qemu-${PACKAGE_ARCH}-static;
 
   echo "Emulators available:" && update-binfmts --display;
    
