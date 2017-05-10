c0cbc5bafb8b4e617f4e728bd15448d951520f05
Jay Rodgers <headmelted@gmail.com>
2017-05-10 10:43:43 +0000

    Passing environment variables manually to docker

--- codebuilds-tools/docker.sh
+++ codebuilds-tools/docker.sh
@@ -5,10 +5,21 @@ docker pull ${DOCKER_IMAGE};
 docker images;
 
 echo "Exporting environment variables...";
-echo $(printenv) > env.list;
 
 echo "Making build script executable...";
 chmod +x ${TRAVIS_BUILD_DIR}/codebuilds-tools/build.sh;
 
 echo "Binding workspace...";
-docker run -it -v ${TRAVIS_BUILD_DIR}:/workspace --env-file ./env.list ${DOCKER_IMAGE} /bin/bash -c /workspace/codebuilds-tools/build.sh;
+docker run -it -v ${TRAVIS_BUILD_DIR}:/workspace \
+    -e "LABEL=${LABEL}" \
+    -e "CROSS_TOOLCHAIN=${CROSS_TOOLCHAIN}" \
+    -e "ARCH=${ARCH}" \
+    -e "NPM_ARCH=${NPM_ARCH}" \
+    -e "GNU_TRIPLET=${GNU_TRIPLET}" \
+    -e "GNU_MULTILIB_TRIPLET=${GNU_MULTILIB_TRIPLET}" \
+    -e "GPP_COMPILER=${GPP_COMPILER}" \
+    -e "GCC_COMPILER=${GCC_COMPILER}" \
+    -e "VSCODE_ELECTRON_PLATFORM=${VSCODE_ELECTRON_PLATFORM}" \
+    -e "PACKAGE_ARCH=${PACKAGE_ARCH}" \
+    -e "QEMU_ARCH=${QEMU_ARCH}" \
+     ${DOCKER_IMAGE} /bin/bash -c /workspace/codebuilds-tools/build.sh;
