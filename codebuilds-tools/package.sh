7e6e60b2cdf9aded4d55367f36cd1f7ef6b4685f
Jay Rodgers <headmelted@gmail.com>
2017-05-12 20:16:54 +0000

    Adding flatpak back in

--- codebuilds-tools/package.sh
+++ codebuilds-tools/package.sh
@@ -10,5 +10,8 @@ gulp "vscode-linux-${PACKAGE_ARCH}-build-deb" | tee -a ../buildlog_${LABEL}.txt;
 echo "Starting vscode-linux-${PACKAGE_ARCH}-build-rpm...";
 gulp "vscode-linux-${PACKAGE_ARCH}-build-rpm" | tee -a ../buildlog_${LABEL}.txt;
 
+echo "Starting vscode-linux-${PACKAGE_ARCH}-build-flatpak...";
+gulp "vscode-linux-${PACKAGE_ARCH}-build-flatpak" | tee -a ../buildlog_${LABEL}.txt;
+
 echo "Tarring build folder for release...";
 tar -zcvf "archive_${LABEL}.tar.gz" .build | tee -a ../buildlog_${LABEL}.txt;
