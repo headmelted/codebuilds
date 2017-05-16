09554819a24562840c4a0acb2c2dcac1fc4fda6f
Jay Rodgers <headmelted@gmail.com>
2017-05-16 14:02:53 +0000

    Removing sudo from gulp commands for package

--- codebuilds-tools/package.sh
+++ codebuilds-tools/package.sh
@@ -2,16 +2,16 @@
 set -e;
 
 echo "Starting vscode-linux-${PACKAGE_ARCH}...";
-sudo gulp "vscode-linux-${PACKAGE_ARCH}";
+gulp "vscode-linux-${PACKAGE_ARCH}";
 
 echo "Starting vscode-linux-${PACKAGE_ARCH}-build-deb...";
-sudo gulp "vscode-linux-${PACKAGE_ARCH}-build-deb";
+gulp "vscode-linux-${PACKAGE_ARCH}-build-deb";
 
 echo "Starting vscode-linux-${PACKAGE_ARCH}-build-rpm...";
-sudo gulp "vscode-linux-${PACKAGE_ARCH}-build-rpm";
+gulp "vscode-linux-${PACKAGE_ARCH}-build-rpm";
 
 echo "Starting vscode-linux-${PACKAGE_ARCH}-build-flatpak...";
-sudo gulp "vscode-linux-${PACKAGE_ARCH}-prepare-flatpak";
+gulp "vscode-linux-${PACKAGE_ARCH}-prepare-flatpak";
 
 # echo "Tarring build folder for release...";
 # tar -zcvf "archive_${LABEL}.tar.gz" .build | tee -a ../buildlog_${LABEL}.txt;
