66cd166a2075f32695c0d6f62211a812bb0f62fa
Jay Rodgers <headmelted@gmail.com>
2017-05-17 19:36:33 +0000

    Changing tee to append. Doh!

--- codebuilds-tools/environment.sh
+++ codebuilds-tools/environment.sh
@@ -23,9 +23,9 @@ else
   
   echo "Adding ${UBUNTU_VERSION} package sources for amd64 and i386...";
   echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION} main universe multiverse restricted" | tee /etc/apt/sources.list;
-  echo "deb [arch=amd64,i386] http://security.ubuntu.com/ubuntu ${UBUNTU_VERSION}-security main universe multiverse restricted" | tee /etc/apt/sources.list;
-  echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-updates main universe multiverse restricted" | tee /etc/apt/sources.list;
-  echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-backports main universe multiverse restricted" | tee /etc/apt/sources.list;
+  echo "deb [arch=amd64,i386] http://security.ubuntu.com/ubuntu ${UBUNTU_VERSION}-security main universe multiverse restricted" | tee -a /etc/apt/sources.list;
+  echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-updates main universe multiverse restricted" | tee -a /etc/apt/sources.list;
+  echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-backports main universe multiverse restricted" | tee -a /etc/apt/sources.list;
   
   echo "Adding ${UBUNTU_VERSION} package sources for ${ARCH}...";
   echo "deb [arch=${ARCH}] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_VERSION} main universe multiverse restricted" | tee -a /etc/apt/sources.list;
@@ -38,6 +38,9 @@ else
   echo "deb-src http://security.ubuntu.com/ubuntu ${UBUNTU_VERSION}-security main universe multiverse restricted" | tee -a /etc/apt/sources.list;
   echo "deb-src http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-updates main universe multiverse restricted" | tee -a /etc/apt/sources.list;
   echo "deb-src http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-backports main universe multiverse restricted" | tee -a /etc/apt/sources.list;
+  
+  echo "Updated sources:";
+  cat /etc/apt/sources.list;
   
   echo "Adding ${ARCH} architecture...";
   dpkg --add-architecture ${ARCH};
