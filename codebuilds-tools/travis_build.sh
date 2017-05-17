27a1b08eda2412f581bcdc226f66c73d565702d5
Jay Rodgers <headmelted@gmail.com>
2017-05-17 11:07:25 +0000

    Updating travis build paths

--- codebuilds-tools/travis_build.sh
+++ codebuilds-tools/travis_build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e;
 
-. ./codebuilds-tools/environment.sh;
-. ./codebuilds-tools/build.sh
+. ./environment.sh;
+. ./build.sh
 if [[ "${LABEL}" == "amd64_linux" ]]; then
-  . ./codebuilds-tools/prepare_tests.sh;
+  . ./prepare_tests.sh;
   echo "Starting test...";
-  ./scripts/test.sh;
+  ../scripts/test.sh;
 fi;
 
 # echo "Starting integration tests...";
 # ./scripts/test-integration.sh;
 
-. ./codebuilds-tools/package.sh;
+. ./package.sh;
