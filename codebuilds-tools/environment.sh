47d32ba36f8089481ad74685d28a46579f1fb528
Jay Rodgers <headmelted@gmail.com>
2017-05-17 15:19:06 +0000

    Adding extended repositories

--- codebuilds-tools/environment.sh
+++ codebuilds-tools/environment.sh
@@ -23,12 +23,21 @@ else
   
   echo "Adding ${UBUNTU_VERSION} package sources for amd64 and i386...";
   echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION} main universe multiverse restricted" | tee /etc/apt/sources.list;
+  echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-security main universe multiverse restricted" | tee /etc/apt/sources.list;
+  echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-updates main universe multiverse restricted" | tee /etc/apt/sources.list;
+  echo "deb [arch=amd64,i386] http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-backports main universe multiverse restricted" | tee /etc/apt/sources.list;
   
   echo "Adding ${UBUNTU_VERSION} package sources for ${ARCH}...";
   echo "deb [arch=${ARCH}] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_VERSION} main universe multiverse restricted" | tee -a /etc/apt/sources.list;
+  echo "deb [arch=${ARCH}] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_VERSION}-security main universe multiverse restricted" | tee -a /etc/apt/sources.list;
+  echo "deb [arch=${ARCH}] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_VERSION}-updates main universe multiverse restricted" | tee -a /etc/apt/sources.list;
+  echo "deb [arch=${ARCH}] http://ports.ubuntu.com/ubuntu-ports ${UBUNTU_VERSION}-backports main universe multiverse restricted" | tee -a /etc/apt/sources.list;
   
   echo "Adding ${UBUNTU_VERSION} package sources for source code...";
   echo "deb-src http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION} main universe multiverse restricted" | tee -a /etc/apt/sources.list;
+  echo "deb-src http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-security main universe multiverse restricted" | tee -a /etc/apt/sources.list;
+  echo "deb-src http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-updates main universe multiverse restricted" | tee -a /etc/apt/sources.list;
+  echo "deb-src http://archive.ubuntu.com/ubuntu ${UBUNTU_VERSION}-backports main universe multiverse restricted" | tee -a /etc/apt/sources.list;
   
   echo "Adding ${ARCH} architecture...";
   dpkg --add-architecture ${ARCH};
