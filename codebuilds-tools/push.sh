13e133733d9522f9bf73059c584f2fde4c0e14ca
Jay Rodgers <headmelted@gmail.com>
2017-05-22 10:28:35 +0000

    Merging for 20170522102832

--- codebuilds-tools/push.sh
+++ codebuilds-tools/push.sh
@@ -6,6 +6,6 @@ git fetch upstream master;
 git merge upstream/master -s recursive -X ours;
 git add .;
 git commit -m "Merging for $merge_tag_id";
-git tag -a `$merge_tag_id` -m "Merging for $merge_tag_id";
+git tag -a "$merge_tag_id" -m "Merging for $merge_tag_id";
 git push origin master;
 git push origin master --tags;
