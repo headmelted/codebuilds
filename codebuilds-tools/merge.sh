395805b43aa4d453e94fb9514c078473f8975ea5
Jay Rodgers <headmelted@gmail.com>
2017-05-22 10:24:24 +0000

    Updating merge instructions

--- codebuilds-tools/merge.sh
+++ codebuilds-tools/merge.sh
@@ -1,8 +1,13 @@
+merge_tag_id=$(date +%Y%m%d%H%M%S);
+chmod 600 deploy-key;
+mv deploy-key ~/.ssh/merge_rsa;
+git remote add master git@github.com:headmelted/codebuilds.git;
+git remote add upstream https://github.com/Microsoft/vscode.git;
 git pull origin master;
 git fetch upstream master;
 git merge upstream/master -s recursive -X ours;
 git add .;
-git commit -m "$1";
-git tag -a `date +%Y%m%d%H%M%S` -m "Merging for +%Y%m%d%H%M%S";
+git commit -m "Merging for $merge_tag_id";
+git tag -a `$merge_tag_id` -m "Merging for $merge_tag_id";
 git push origin master;
 git push origin master --tags;
