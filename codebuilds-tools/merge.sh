318177f666dbdce39b87d7cb794ef0b70e494a31
Jay Rodgers <headmelted@gmail.com>
2017-05-22 10:34:24 +0000

    Adding merge via Travis

--- codebuilds-tools/merge.sh
+++ codebuilds-tools/merge.sh
@@ -4,4 +4,13 @@ chmod 600 deploy-key;
 mv deploy-key ~/.ssh/merge_rsa;
 git remote add master git@github.com:headmelted/codebuilds.git;
 git remote add upstream https://github.com/Microsoft/vscode.git;
-. ./push.sh;
+merge_tag_id=$(date +%Y%m%d%H%M%S);
+git pull origin master;
+git fetch upstream master;
+git merge upstream/master -s recursive -X ours;
+git add .;
+git commit -m "Merging for $merge_tag_id";
+git tag -a "$merge_tag_id" -m "Merging for $merge_tag_id";
+git push origin master;
+git push origin master --tags;
+
--- codebuilds-tools/push.sh
+++ codebuilds-tools/push.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e;
-merge_tag_id=$(date +%Y%m%d%H%M%S);
 git pull origin master;
 git fetch upstream master;
 git merge upstream/master -s recursive -X ours;
 git add .;
-git commit -m "Merging for $merge_tag_id";
-git tag -a "$merge_tag_id" -m "Merging for $merge_tag_id";
+git commit -m "$1";
 git push origin master;
 git push origin master --tags;
