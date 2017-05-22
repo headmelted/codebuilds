0c43e397f99cbfecfce51ffcdfd7b66e37803be2
Jay Rodgers <headmelted@gmail.com>
2017-05-22 10:06:09 +0000

    Adding changes to master

--- codebuilds-tools/merge.sh
+++ codebuilds-tools/merge.sh
@@ -3,6 +3,6 @@ git fetch upstream master;
 git merge upstream/master -s recursive -X ours;
 git add .;
 git commit -m "$1";
-git tag -a `date +%Y%m%d%H%M%S` -m "$1";
+git tag -a `date +%Y%m%d%H%M%S` -m "Merging for +%Y%m%d%H%M%S";
 git push origin master;
 git push origin master --tags;
