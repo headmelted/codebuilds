ff1c54c6ff693d4334551db97754b1e6b7fa613b
Jay Rodgers <headmelted@gmail.com>
2017-05-22 09:12:43 +0000

    Adding xvfb back in

--- codebuilds-tools/install_package_and_test.sh
+++ codebuilds-tools/install_package_and_test.sh
@@ -1,4 +1,5 @@
 dpkg -i $(find .build/linux -type f -name "*.deb");
 echo "Detecting code-oss...";
 which code-oss;
-test/electron/index.js $(which code-oss);
+. ./codebuilds-tools/startxvfb.sh;
+$(which code-oss) test/electron/index.js $@;
