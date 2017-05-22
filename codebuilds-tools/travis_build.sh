02f4ddf123ec20d85b0366180e4d58cde8b95d5e
Jay Rodgers <headmelted@gmail.com>
2017-05-22 07:35:56 +0000

    Adding updated build scripts

--- codebuilds-tools/travis_build.sh
+++ codebuilds-tools/travis_build.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 set -e;
 
-cd ~/workspace;
+cd /workspace;
 
-echo "Listing workspace...";
-ls .
-
-. ~/workspace/codebuilds-tools/environment.sh;
-. ~/workspace/codebuilds-tools/build.sh;
-# . /workspace/codebuilds-tools/startxvfb.sh;
-
-# if [ "${LABEL}" == "amd64_linux" ]; then
-#   echo "Starting test...";
-#   /workspace/scripts/test.sh;
-# else
-#   if [ "${LABEL}" == "armhf_linux" ]; then
-#     echo "Starting emulated test...";
-#     . /workspace/codebuilds-tools/emulate.sh "scripts/test.sh";
-#   fi;
-# fi;
-
-# echo "Starting integration tests...";
-# ./scripts/test-integration.sh;
-
-. ~/workspace/codebuilds-tools/package.sh;
-# . /workspace/codebuilds-tools/flatpak/build.sh;
+. ./codebuilds-tools/all.sh;
