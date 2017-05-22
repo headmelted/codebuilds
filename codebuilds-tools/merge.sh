1674efc36791057cc54d5abfbd05340b474268e3
Jay Rodgers <headmelted@gmail.com>
2017-05-22 10:27:21 +0000

    Merging for

--- codebuilds-tools/merge.sh
+++ codebuilds-tools/merge.sh
@@ -1,13 +1,8 @@
+#!/bin/bash
+set -e;
 merge_tag_id=$(date +%Y%m%d%H%M%S);
 chmod 600 deploy-key;
 mv deploy-key ~/.ssh/merge_rsa;
 git remote add master git@github.com:headmelted/codebuilds.git;
 git remote add upstream https://github.com/Microsoft/vscode.git;
-git pull origin master;
-git fetch upstream master;
-git merge upstream/master -s recursive -X ours;
-git add .;
-git commit -m "Merging for $merge_tag_id";
-git tag -a `$merge_tag_id` -m "Merging for $merge_tag_id";
-git push origin master;
-git push origin master --tags;
+. ./push.sh;
