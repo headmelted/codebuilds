1eb4c81f3272d2a6965ee3a5636a57a029755d80
Jay Rodgers <headmelted@gmail.com>
2017-05-23 10:27:04 +0000

    Removing root from test as it was causing issues for Chromium

--- codebuilds-tools/emulate.sh
+++ codebuilds-tools/emulate.sh
@@ -3,4 +3,3 @@ set -e;
 
 echo "Booting ${QEMU_ARCH} emulator...";
 qemu-system-${QEMU_ARCH} -nographic -serial mon:stdio -machine virt -cpu ${QEMU_CPU} -m ${QEMU_MEMORY} -dtb boot.dtb -kernel boot.img -sd image.img -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2,script=$1";
-
--- codebuilds-tools/install_package_and_test.sh
+++ codebuilds-tools/install_package_and_test.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -e;
 
-# DANGER!
-# THIS SCRIPT IS CACHED AT TRAVIS CI.  CHANGING THIS FILE WILL REQUIRE THAT THE CACHE IS REBUILT!!!!!
-
-dpkg -i $(find .build/linux -type f -name "*.deb");
+echo "Installing deb...";
+sudo dpkg -i $(find .build/linux -type f -name '*.deb');
+    
 echo "Detecting code-oss...";
 which code-oss;
 . ./codebuilds-tools/startxvfb.sh;
-echo "Creating dummy user data dir...";
-mkdir dummy;
-$(which code-oss) --user-data-dir=./dummy test/electron/index.js $@;
+$(which code-oss) test/electron/index.js $@;
