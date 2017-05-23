7fbdd392a18b3b38fd333a617bd4c6c11ea55572
Jay Rodgers <headmelted@gmail.com>
2017-05-23 08:21:25 +0000

    Switching emulated Pi to 3b instead of 2b due to boot issues inside QEMU

--- codebuilds-tools/emulate.sh
+++ codebuilds-tools/emulate.sh
@@ -2,5 +2,5 @@
 set -e;
 
 echo "Booting Raspberry Pi 2...";
-qemu-system-arm -nographic -serial mon:stdio -M raspi2 -dtb bcm2709-rpi-2-b.dtb -kernel kernel7.img -sd image.img -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2,script=$1";
+qemu-system-arm -nographic -serial mon:stdio -M raspi2 -dtb bcm2710-rpi-3-b.dtb -kernel kernel7.img -sd image.img -append "rw earlyprintk loglevel=8 console=ttyAMA0,115200 dwc_otg.lpm_enable=0 root=/dev/mmcblk0p2,script=$1";
 
