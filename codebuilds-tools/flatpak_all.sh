b25d540b35eeadeac15f9f1428698a85e9583526
Jay Rodgers <headmelted@gmail.com>
2017-05-24 15:12:29 +0000

    Removing flatpak from user mode

--- codebuilds-tools/flatpak_all.sh
+++ codebuilds-tools/flatpak_all.sh
@@ -7,11 +7,11 @@ apt-get install -y qemu-system-${QEMU_ARCH} qemu-user-static flatpak;
 # echo "Restarting systemd-binfmt.service daemon...";
 # systemctl restart systemd-binfmt.service;
 
-echo "Installing flatpak runtimes for ...";
-flatpak install --user gnome org.freedesktop.Sdk/${QEMU_ARCH}/1.6 org.freedesktop.Platform/${QEMU_ARCH}/1.6;
+echo "Installing flatpak runtimes for ${QEMU_ARCH}...";
+flatpak install gnome org.freedesktop.Sdk/${QEMU_ARCH}/1.6 org.freedesktop.Platform/${QEMU_ARCH}/1.6;
 
 echo "Initializing flatpak directory...";
-flatpak build-init /workspace/flatpak code-oss org.freedesktop.Sdk/${QEMU_ARCH}  org.freedesktop.Platform/${QEMU_ARCH};
+flatpak build-init /workspace/flatpak code-oss org.freedesktop.Sdk/${QEMU_ARCH} org.freedesktop.Platform/${QEMU_ARCH};
 
 echo "Executing the build inside the flatpak sandbox...";
 flatpak build code-oss ln -s ./ /workspace && . /workspace/codebuilds-tools/setup_nvm.sh && . /workspace/codebuilds-tools/build.sh;
