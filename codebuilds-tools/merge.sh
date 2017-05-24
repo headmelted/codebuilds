35a3724c6880f1de67a2eb932ee29e3a4a4c4db6
Jay Rodgers <headmelted@gmail.com>
2017-05-24 09:04:36 +0000

    Adding encrypted merge key

--- codebuilds-tools/merge.sh
+++ codebuilds-tools/merge.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e;
 cd /workspace;
-chmod 600 deploy_key;
-mv deploy_key ~/.ssh/merge_rsa;
+openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv -in merger_rsa.enc -out merger_rsa -d;
+chmod 600 merger_rsa;
+mv merger_rsa ~/.ssh/merge_rsa;
 git remote add master git@github.com:headmelted/codebuilds.git;
 git remote add upstream https://github.com/Microsoft/vscode.git;
 merge_tag_id=$(date +%Y%m%d%H%M%S);
