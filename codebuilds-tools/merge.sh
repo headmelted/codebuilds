7622efc6a68686ae575eec3f589b9036e7d78236
Jay Rodgers <headmelted@gmail.com>
2017-05-24 09:51:15 +0000

    Updating merge logic

--- codebuilds-tools/merge.sh
+++ codebuilds-tools/merge.sh
@@ -1,18 +1,47 @@
 #!/bin/bash
 set -e;
+
 cd /workspace;
+
+echo "Decrypting merger_rsa.enc...";
 openssl aes-256-cbc -K $encrypted_26b4962af0e7_key -iv $encrypted_26b4962af0e7_iv -in merger_rsa.enc -out merger_rsa -d;
+
+echo "Changing permissions of merger_rsa...";
 chmod 600 merger_rsa;
-mv merger_rsa ~/.ssh/merge_rsa;
-git remote add master git@github.com:headmelted/codebuilds.git;
+
+echo "Moving merger_rsa to ~/.ssh/merger_rsa...";
+mv merger_rsa ~/.ssh/merger_rsa;
+
+echo "Adding origin...";
+git remote add origin git@github.com:headmelted/codebuilds.git;
+
+echo "Adding upstream...";
 git remote add upstream https://github.com/Microsoft/vscode.git;
+
 merge_tag_id=$(date +%Y%m%d%H%M%S);
+echo "Merge tag is $merge_tag_id.";
+
+echo "Pulling from origin...";
 git pull origin master;
+
+echo "Fetching upstream...";
 git fetch upstream master;
+
+echo "Merging upstream onto origin with a preference of ours...";
 git merge upstream/master -s recursive -X ours;
+
+echo "Staging all changes...";
 git add .;
+
+echo "Committing changes...";
 git commit -m "Merging for $merge_tag_id";
+
+echo "Tagging changes with $merge_tag_id...";
 git tag -a "$merge_tag_id" -m "Merging for $merge_tag_id";
+
+echo "Pushing changes back to origin...";
 git push origin master;
+
+echo "Pushing tags to origin...";
 git push origin master --tags;
 
