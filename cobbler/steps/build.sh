b3014aa9c0f4d896ffa076c760d130d08e45a317
Jay Rodgers <headmelted@gmail.com>
2017-08-15 08:58:12 +0100

    Update build.sh
    Updating VSCODE_ELECTRON_PLATFORM logic, needs exported

--- cobbler/steps/build.sh
+++ cobbler/steps/build.sh
@@ -1,18 +1,7 @@
 #!/bin/bash
 set -e;
 
-echo "VS Code electron platform before set: $VSCODE_ELECTRON_PLATFORM";
-
-case "$ARCH" in
-  "armhf")
-    VSCODE_ELECTRON_PLATFORM="arm";;
-  "amd64")
-    VSCODE_ELECTRON_PLATFORM="amd64";;
-esac
-
-echo "VS Code electron platform after set but before export: $VSCODE_ELECTRON_PLATFORM";
-export VSCODE_ELECTRON_PLATFORM="$VSCODE_ELECTRON_PLATFORM";
-echo "VS Code electron platform after export: $VSCODE_ELECTRON_PLATFORM";
+echo "VS Code electron platform: $VSCODE_ELECTRON_PLATFORM";
 
 echo "Running npm install for ${NPM_ARCH}";
 ./scripts/npm.sh install --arch=${NPM_ARCH} --unsafe-perm;
