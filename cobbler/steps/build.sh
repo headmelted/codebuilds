b36eae8f22ae85bc61b5e3add56c25313b0717df
Jay Rodgers <headmelted@gmail.com>
2018-07-09 09:35:59 +0100

    Updating to match yarn-based builds at Microsoft

--- cobbler/steps/build.sh
+++ cobbler/steps/build.sh
@@ -4,16 +4,8 @@ set -e;
 echo "VS Code electron platform: $VSCODE_ELECTRON_PLATFORM";
 
 echo "Running npm install for ${NPM_ARCH}";
-./scripts/npm.sh install --arch=${NPM_ARCH} --unsafe-perm;
+./scripts/npm.sh install --target-arch=${NPM_ARCH} --unsafe-perm;
 
-echo "Starting hygiene";
-#gulp hygiene;
+yarn run gulp vscode-linux-arm-min
 
-echo "Starting electron";
-gulp electron-${NPM_ARCH};
-
-echo "Starting compile";
-gulp compile --max_old_space_size=4096;
-
-echo "Starting optimize";
-gulp optimize-vscode --max_old_space_size=4096;
+yarn run gulp vscode-linux-arm-build-deb
