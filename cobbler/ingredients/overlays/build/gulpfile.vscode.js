d2917bc0f047ed1401436ed8d27fe5fcf2f0ba07
Jay Rodgers <headmelted@gmail.com>
2018-09-09 16:00:12 +0100

    Adding armv7l and arm64
    This naming is consistent with how electron now distinguishes ARM versions

--- cobbler/ingredients/overlays/build/gulpfile.vscode.js
+++ cobbler/ingredients/overlays/build/gulpfile.vscode.js
@@ -166,8 +166,8 @@ gulp.task('clean-electron', util.rimraf('.build/electron'));
 gulp.task('electron', ['clean-electron'], getElectron(process.arch));
 gulp.task('electron-ia32', ['clean-electron'], getElectron('ia32'));
 gulp.task('electron-x64', ['clean-electron'], getElectron('x64'));
-gulp.task('electron-arm', ['clean-electron'], getElectron('arm'));
-
+gulp.task('electron-armv7l', ['clean-electron'], getElectron('armv7l'));
+gulp.task('electron-arm64', ['clean-electron'], getElectron('arm64'));
 
 /**
  * Compute checksums for some files.
