bca5ef5340d174dadd541d6cd99e3659aa8a7274
Jay Rodgers <headmelted@gmail.com>
2018-09-09 14:46:55 +0100

    Update package.sh

--- cobbler/steps/package.sh
+++ cobbler/steps/package.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 set -e;
 
-echo "Starting vscode-linux-${PACKAGE_ARCH}";
-gulp "vscode-linux-${PACKAGE_ARCH}";
+echo "Starting vscode-linux-${NPM_ARCH}";
+gulp "vscode-linux-${NPM_ARCH}";
 
-echo "Starting vscode-linux-${PACKAGE_ARCH}-build-deb";
-gulp "vscode-linux-${PACKAGE_ARCH}-build-deb";
+echo "Starting vscode-linux-${NPM_ARCH}-build-deb";
+yarn run gulp vscode-linux-${NPM_ARCH}-build-deb
 
-echo "Starting vscode-linux-${PACKAGE_ARCH}-build-rpm";
-gulp "vscode-linux-${PACKAGE_ARCH}-build-rpm";
+echo "Starting vscode-linux-${NPM_ARCH}-build-rpm";
+yarn run gulp "vscode-linux-${NPM_ARCH}-build-rpm";
 
-echo "Starting vscode-linux-${PACKAGE_ARCH}-flatpak";
-gulp --verbose "vscode-linux-${PACKAGE_ARCH}-flatpak";
+echo "Starting vscode-linux-${NPM_ARCH}-flatpak";
+yarn run gulp --verbose "vscode-linux-${NPM_ARCH}-flatpak";
 
 echo "Tarring build folder for release";
-tar -zcvf "../code-oss_${LABEL}.tar.gz" ../VSCode-linux-${PACKAGE_ARCH};
-
-
+tar -zcvf "../code-oss_${LABEL}.tar.gz" ../VSCode-linux-${NPM_ARCH};
