9d18bd54321c0ebe13baf5bdafaa56539a1654b8
Jay Rodgers <headmelted@gmail.com>
2018-09-11 08:46:38 +0100

    Update package.sh

--- cobbler/steps/package.sh
+++ cobbler/steps/package.sh
@@ -18,10 +18,10 @@ tree /kitchen/.build/;
 tree /kitchen/.builds/;
 
 echo "Moving deb packages for release";
-mv "/kitchen/.builds/${ARCH}/.code/.build/linux/deb/${ARCH}/deb/*.deb" /cobbler/.output/;
+mv "/kitchen/.builds/**/*.deb" /cobbler/.output/;
 
 echo "Moving rpm packages for release";
-mv "/kitchen/.builds/${ARCH}/.code/.build/linux/rpm/${ARCH}/rpmbuild/RPMS/${ARCH}/*.rpm" /cobbler/.output/;
+mv "/kitchen/.builds/**/*.rpm" /cobbler/.output/;
 
 #echo "Starting vscode-linux-${NPM_ARCH}-flatpak";
 #yarn run gulp --verbose "vscode-linux-${NPM_ARCH}-flatpak";
