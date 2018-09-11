cfd206a26181b30e875d1a77fe44c0afcd535ed1
Jay Rodgers <headmelted@gmail.com>
2018-09-11 09:54:04 +0100

    Update package.sh

--- cobbler/steps/package.sh
+++ cobbler/steps/package.sh
@@ -16,18 +16,19 @@ yarn run gulp "vscode-linux-${NPM_ARCH}-build-rpm";
 echo "Looking for release debs";
 tree /kitchen/.build/;
 tree /kitchen/.builds/;
+tree /kitchen/.builds/${ARCH}/.code/.build/;
 
 echo "Moving deb packages for release";
-mv "/kitchen/.builds/**/*.deb" /cobbler/.output/;
+mv "/kitchen/.builds/${ARCH}/.code/.build/**/*.deb" /cobbler/.output/;
 
 echo "Moving rpm packages for release";
-mv "/kitchen/.builds/**/*.rpm" /cobbler/.output/;
+mv "/kitchen/.builds/${ARCH}/.code/.build/**/*.rpm" /cobbler/.output/;
 
 #echo "Starting vscode-linux-${NPM_ARCH}-flatpak";
 #yarn run gulp --verbose "vscode-linux-${NPM_ARCH}-flatpak";
 
 echo "Tarring build folder for release";
-tar -zcvf "/cobbler/.output/code-oss_${LABEL}.tar.gz" ../VSCode-linux-${NPM_ARCH};
+tar -zcvf "/cobbler/.output/code-oss_${LABEL}.tar.gz" /kitchen/.builds/VSCode-linux-${NPM_ARCH};
 
 echo "Listing output packages";
 ls /cobbler/.output;
