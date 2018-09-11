9b69f97cd2a9bcfc005e0f64f0598b3c12224a3e
Jay Rodgers <headmelted@gmail.com>
2018-09-11 07:48:38 +0100

    Update package.sh

--- cobbler/steps/package.sh
+++ cobbler/steps/package.sh
@@ -10,17 +10,20 @@ mkdir /cobbler/.output;
 echo "Starting vscode-linux-${NPM_ARCH}-build-deb";
 yarn run gulp vscode-linux-${NPM_ARCH}-build-deb;
 
-echo "Moving deb packages for release";
-mv "/kitchen/.builds/${ARCH}/.code/.build/linux/deb/${ARCH}/deb/*.deb" /cobbler/.output/;
-
 echo "Starting vscode-linux-${NPM_ARCH}-build-rpm";
 yarn run gulp "vscode-linux-${NPM_ARCH}-build-rpm";
+
+echo "Looking for release debs";
+tree /kitchen;
+
+echo "Moving deb packages for release";
+mv "/kitchen/.builds/${ARCH}/.code/.build/linux/deb/${ARCH}/deb/*.deb" /cobbler/.output/;
 
 echo "Moving rpm packages for release";
 mv "/kitchen/.builds/${ARCH}/.code/.build/linux/rpm/${ARCH}/rpmbuild/RPMS/${ARCH}/*.rpm" /cobbler/.output/;
 
-echo "Starting vscode-linux-${NPM_ARCH}-flatpak";
-yarn run gulp --verbose "vscode-linux-${NPM_ARCH}-flatpak";
+#echo "Starting vscode-linux-${NPM_ARCH}-flatpak";
+#yarn run gulp --verbose "vscode-linux-${NPM_ARCH}-flatpak";
 
 echo "Tarring build folder for release";
 tar -zcvf "/cobbler/.output/code-oss_${LABEL}.tar.gz" ../VSCode-linux-${NPM_ARCH};
