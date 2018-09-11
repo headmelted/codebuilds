840ebbf71e25ace03465b0f2879e6f389c137d13
Jay Rodgers <headmelted@gmail.com>
2018-09-11 13:20:37 +0100

    Update build.sh

--- cobbler/steps/build.sh
+++ cobbler/steps/build.sh
@@ -3,9 +3,6 @@ set -e;
 
 echo "VS Code electron platform: $VSCODE_ELECTRON_PLATFORM";
 
-echo "Length of GITHUB_TOKEN: ${#GITHUB_TOKEN}";
-echo "GITHUB_TOKEN: $GITHUB_TOKEN";
-
 echo "Running npm install for ${NPM_ARCH}";
 ./scripts/npm.sh install --target-arch=${NPM_ARCH} --unsafe-perm;
 
