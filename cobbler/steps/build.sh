2ca2a608f7588f227a54a986a3ac1aaf5c5be386
Jay Rodgers <headmelted@gmail.com>
2018-09-11 12:08:53 +0100

    Update build.sh

--- cobbler/steps/build.sh
+++ cobbler/steps/build.sh
@@ -3,9 +3,9 @@ set -e;
 
 echo "VS Code electron platform: $VSCODE_ELECTRON_PLATFORM";
 
+echo "Length of GITHUB_TOKEN: ${#GITHUB_TOKEN}";
+
 echo "Running npm install for ${NPM_ARCH}";
 ./scripts/npm.sh install --target-arch=${NPM_ARCH} --unsafe-perm;
 
 yarn run gulp vscode-linux-${NPM_ARCH}-min;
-
-echo "Length of GITHUB_TOKEN: ${#GITHUB_TOKEN}";
