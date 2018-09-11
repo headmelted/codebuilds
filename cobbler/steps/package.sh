c3e2cbce4b91bfe2f413e6147d56fb4416b31221
Jay Rodgers <headmelted@gmail.com>
2018-09-11 15:50:50 +0100

    Update package.sh

--- cobbler/steps/package.sh
+++ cobbler/steps/package.sh
@@ -7,26 +7,32 @@ gulp "vscode-linux-${NPM_ARCH}";
 echo "Creating output folder";
 mkdir /cobbler/.output;
 
+echo "Starting vscode-linux-${NPM_ARCH}-prepare-deb";
+yarn run gulp vscode-linux-${NPM_ARCH}-prepare-deb;
+
 echo "Starting vscode-linux-${NPM_ARCH}-build-deb";
 yarn run gulp vscode-linux-${NPM_ARCH}-build-deb;
-
-echo "Starting vscode-linux-${NPM_ARCH}-build-rpm";
-yarn run gulp "vscode-linux-${NPM_ARCH}-build-rpm";
-
-echo "Looking for release debs";
-tree /kitchen/.builds/${ARCH}/.code/.build/;
 
 echo "Moving deb packages for release";
 mv /kitchen/.builds/${ARCH}/.code/.build/linux/deb/${ARCH}/deb/*.deb /cobbler/.output/;
 
+echo "Starting vscode-linux-${NPM_ARCH}-prepare-rpm";
+yarn run gulp "vscode-linux-${NPM_ARCH}-prepare-rpm";
+
+echo "Starting vscode-linux-${NPM_ARCH}-build-rpm";
+yarn run gulp "vscode-linux-${NPM_ARCH}-build-rpm";
+
 echo "Moving rpm packages for release";
 mv /kitchen/.builds/${ARCH}/.code/.build/linux/rpm/${RPM_ARCH}/rpmbuild/RPMS/${RPM_ARCH}/*.rpm /cobbler/.output/;
 
-# echo "Starting vscode-linux-${NPM_ARCH}-flatpak";
-# yarn run gulp --verbose "vscode-linux-${NPM_ARCH}-flatpak";
+echo "Starting vscode-linux-${NPM_ARCH}-prepare-snap";
+yarn run gulp --verbose "vscode-linux-${NPM_ARCH}-prepare-snap";
 
-# echo "Starting vscode-linux-${NPM_ARCH}-snap";
-# yarn run gulp --verbose "vscode-linux-${NPM_ARCH}-snap";
+echo "Starting vscode-linux-${NPM_ARCH}-build-snap";
+yarn run gulp --verbose "vscode-linux-${NPM_ARCH}-build-snap";
+
+echo "Looking for additional release files";
+tree /kitchen/.builds/${ARCH}/.code/.build/;
 
 # echo "Tarring build folder for release";
 # tar -zcvf "/cobbler/.output/code-oss_${LABEL}.tar.gz" /kitchen/.builds/VSCode-linux-${NPM_ARCH};
