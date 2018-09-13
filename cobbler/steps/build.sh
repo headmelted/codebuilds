f8fbbcb90d6d6a36f96b4adbb18e17b4e21e4544
Jay Rodgers <headmelted@gmail.com>
2018-09-13 11:35:57 +0100

    Update build.sh

--- cobbler/steps/build.sh
+++ cobbler/steps/build.sh
@@ -3,6 +3,10 @@ set -e;
 
 echo "VS Code electron platform: $VSCODE_ELECTRON_PLATFORM";
 
+echo "Current directory is [$(pwd)]";
+echo "Directory listing:"
+tree .;
+
 echo "Running npm install for ${NPM_ARCH}";
 ./scripts/npm.sh install --target-arch=${NPM_ARCH} --unsafe-perm;
 
