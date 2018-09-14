3b503801c342f12b7caf91e6eb071ed3cff8697b
Jay Rodgers <headmelted@gmail.com>
2018-09-14 20:16:06 +0100

    Update build.sh

--- cobbler/steps/build.sh
+++ cobbler/steps/build.sh
@@ -5,8 +5,8 @@ echo "VS Code electron platform: $VSCODE_ELECTRON_PLATFORM";
 
 echo "Current directory is [$(pwd)]";
 
-echo "Running yarn install for ${NPM_ARCH}";
-yarn install --target-arch=${NPM_ARCH} --unsafe-perm;
+echo "Running npm install for ${NPM_ARCH}";
+npm install --target-arch=${NPM_ARCH} --unsafe-perm;
 
 echo "Compiling VS Code for $NPM_ARCH";
 yarn run gulp vscode-linux-${NPM_ARCH}-min;
