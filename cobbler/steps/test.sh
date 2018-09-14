618d8bf01db0d1389b0f6569ec56a49fefb1d5f6
Jay Rodgers <headmelted@gmail.com>
2018-09-14 10:12:17 +0100

    Update test.sh

--- cobbler/steps/test.sh
+++ cobbler/steps/test.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 set -e;
 
-echo "Creating images directory [/kitchen/.images]"
-mkdir /kitchen/.images;
-
-echo "Downloading Ubuntu cloud image for $ARCH";
-wget "https://cloud-images.ubuntu.com/cosmic/20180913/cosmic-server-cloudimg-$ARCH.img" -O /kitchen/.images/cosmic-server-cloudimg-$ARCH.img;
-
-echo "Downloaded cosmic-server-cloudimg-$ARCH.img successfully."
-
 echo "Installing QEMU";
-apt-get install -y qemu;
+apt-get install -y qemu-system-$QEMU_PACKAGE_ARCH;
 
 echo "Booting QEMU image"
 qemu-system-$QEMU_ARCH -m 4096 -M virt -nographic -drive if=none,file=/kitchen/.images/cosmic-server-cloudimg-$ARCH.img,id=hd0 \
