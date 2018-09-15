720dac1293231d10bf96aa0fb58357f98d38466a
Jay Rodgers <headmelted@gmail.com>
2018-09-15 21:31:32 +0100

    Update test.sh

--- cobbler/steps/test.sh
+++ cobbler/steps/test.sh
@@ -6,8 +6,14 @@ echo "Cobbler is $(lsb_release -a)";
 echo "Installed packages list:"
 apt list --installed;
 
+echo "Creating images directory [/kitchen/.images]"
+mkdir /kitchen/.images;
+
+echo "Downloading Ubuntu cloud images (used for testing later)";
+wget "https://cloud-images.ubuntu.com/cosmic/current/cosmic-server-cloudimg-$arch.img" -O ./kitchen/.images/cosmic-server-cloudimg-$arch.img;
+
 echo "Booting QEMU image"
-/usr/bin/qemu-system-$QEMU_ARCH -m 4096 -M virt -nographic -drive if=none,file=/kitchen/.images/cosmic-server-cloudimg-$ARCH.img,id=hd0 \
+/usr/bin/qemu-system-$QEMU_ARCH -m 4096 -M virt -nographic -drive if=none,file=/kitchen/.images/cosmic-server-cloudimg-$arch,id=hd0 \
  -device virtio-blk-device,drive=hd0 -netdev type=tap,id=net0 -device virtio-net-device,netdev=net0;
     
 # echo "Running tests";
