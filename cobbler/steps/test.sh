0bcedc97e991f0c5525b69aa87e60938e6b63266
Jay Rodgers <headmelted@gmail.com>
2018-09-15 22:36:18 +0100

    Update test.sh

--- cobbler/steps/test.sh
+++ cobbler/steps/test.sh
@@ -13,7 +13,7 @@ echo "Downloading Ubuntu cloud images (used for testing later)";
 wget "https://cloud-images.ubuntu.com/cosmic/current/cosmic-server-cloudimg-$arch.img" -O /kitchen/.images/cosmic-server-cloudimg-$arch.img;
 
 echo "Creating TUN device for the session"
-sudo tunctl -t tap0 -u me
+tunctl -t tap0 -u me
 
 echo "Booting QEMU image"
 /usr/bin/qemu-system-$QEMU_ARCH -m 4096 -M virt -nographic -drive if=none,file=/kitchen/.images/cosmic-server-cloudimg-$arch,id=hd0 -device virtio-blk-device,drive=hd0 -netdev tap,ifname=tap0,id=mynet0,script=no;
