bbe280226c4aa0a39767ac6b19531256b50542dc
Jay Rodgers <headmelted@gmail.com>
2018-09-15 22:24:00 +0100

    Update test.sh

--- cobbler/steps/test.sh
+++ cobbler/steps/test.sh
@@ -13,7 +13,7 @@ echo "Downloading Ubuntu cloud images (used for testing later)";
 wget "https://cloud-images.ubuntu.com/cosmic/current/cosmic-server-cloudimg-$arch.img" -O /kitchen/.images/cosmic-server-cloudimg-$arch.img;
 
 echo "Booting QEMU image"
-/usr/bin/qemu-system-$QEMU_ARCH -m 4096 -M virt -nographic -drive if=none,file=/kitchen/.images/cosmic-server-cloudimg-$arch,id=hd0 -device virtio-blk-device,drive=hd0 -net nic,model=virtio -net tap,ifname=tap0,script=no,downscript=no;
+/usr/bin/qemu-system-$QEMU_ARCH -m 4096 -M virt -nographic -drive if=none,file=/kitchen/.images/cosmic-server-cloudimg-$arch,id=hd0 -device virtio-blk-device,drive=hd0 -net nic,macaddr=18:27:99:aa:de:01,model=virtio -net tap;
     
 # echo "Running tests";
 # ./scripts/test.sh;
