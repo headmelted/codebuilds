993603e3728a74dd0f96389c635c655947d0d76b
Jay Rodgers <headmelted@gmail.com>
2018-09-15 12:07:33 +0100

    Update test.sh

--- cobbler/steps/test.sh
+++ cobbler/steps/test.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 set -e;
 
-which qemu-system-$QEMU_ARCH;
-
 echo "Booting QEMU image"
-qemu-system-$QEMU_ARCH -m 4096 -M virt -nographic -drive if=none,file=/kitchen/.images/cosmic-server-cloudimg-$ARCH.img,id=hd0 \
+/usr/bin/qemu-system-$QEMU_ARCH -m 4096 -M virt -nographic -drive if=none,file=/kitchen/.images/cosmic-server-cloudimg-$ARCH.img,id=hd0 \
  -device virtio-blk-device,drive=hd0 -netdev type=tap,id=net0 -device virtio-net-device,netdev=net0;
     
 # echo "Running tests";
