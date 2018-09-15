0b3588173f11044c7050a523ece56c6627094de4
Jay Rodgers <headmelted@gmail.com>
2018-09-15 12:12:51 +0100

    Update test.sh

--- cobbler/steps/test.sh
+++ cobbler/steps/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e;
 
+echo "Installed packages list:"
+apt-get list installed
+
 echo "Booting QEMU image"
 /usr/bin/qemu-system-$QEMU_ARCH -m 4096 -M virt -nographic -drive if=none,file=/kitchen/.images/cosmic-server-cloudimg-$ARCH.img,id=hd0 \
  -device virtio-blk-device,drive=hd0 -netdev type=tap,id=net0 -device virtio-net-device,netdev=net0;
