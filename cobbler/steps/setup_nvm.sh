39f1843147109510516ad70a080acb7bf0f4cb37
Jay Rodgers <headmelted@gmail.com>
2018-09-20 16:05:08 +0100

    Create setup_nvm.sh

---
+++ cobbler/steps/setup_nvm.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e;
+
+echo "Removing old nvm for user if it exists";
+rm -rf ~/.nvm;
+
+echo "Cloning nvm for user";
+git clone --depth 1 https://github.com/creationix/nvm.git ~/.nvm;
+
+echo "Setting current owner as owner of ~/.nvm";
+chown ${USER:=$(/usr/bin/id -run)}:$USER -R ~/.nvm;
+
+echo "Running NVM script";
+source ~/.nvm/nvm.sh;
+
+echo "Setting up NVM";
+nvm install $1;
+nvm use $1;
+nvm alias default $1;
+  
+echo "Setting python binding";
+npm config set python `which python`;
+
+echo "Installing common npm dependencies";
+npm install -g gulp yarn;
