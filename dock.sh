f71b73877d14b8f11e2b6e5a32dc72179c0b5c46
Jay Rodgers <headmelted@gmail.com>
2018-09-20 14:38:11 +0100

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -2,9 +2,8 @@
 
 # dock.sh
 #
-# Run the given script in the given container, passing in our
-# environment variables.  THE BASH SCRIPT MUST BE A RELATIVE
-# PATH.
+# Run the given script in the loaded container, passing in our
+# environment variables.  THE BASH SCRIPT MUST BE IN THE /cobbler PATH.
 #
 # usage: dock.sh <docker_hub_image> <bash_script_to_run_in_container>
 
@@ -20,6 +19,7 @@ echo "Binding workspace and executing script";
 docker run -it --security-opt apparmor:unconfined --cap-add SYS_ADMIN \
 -e GITHUB_TOKEN=$GITHUB_TOKEN \
 -e COBBLER_GIT_ENDPOINT=$COBBLER_GIT_ENDPOINT \
+-e COBBLER_SCRIPT='$2' \
 -v $(pwd)/cobbler:/root/cobbler \
 -v $(pwd)/output:/root/output \
 $docker_image;
