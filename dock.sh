1e244bfe5fe2cc228682f1a9247dc28c66af19a3
Jay Rodgers <headmelted@gmail.com>
2018-09-20 14:35:16 +0100

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -14,21 +14,7 @@ echo "Checking if $docker_image exists locally"
 if [[ "$(docker images -q $docker_image 2> /dev/null)" != "" ]]; then
   echo "$docker_image does not exist locally, retrieving from hub"
   docker pull $docker_image;
-fi
-
-docker images; 
-
-echo "Current directory is [$(pwd)]"
-
-echo "Directory list:"
-ls;
-
-echo "Creating output directory (./output)";
-mkdir output;
-
-echo "------ HOST DETAILS ------";
-lsb_release -a;
-echo "--------------------------";
+fi;
 
 echo "Binding workspace and executing script";
 docker run -it --security-opt apparmor:unconfined --cap-add SYS_ADMIN \
