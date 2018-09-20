61ad106370a53c0df2912219ef224c3af8bb6c5a
Jay Rodgers <headmelted@gmail.com>
2018-09-20 19:38:28 +0100

    Update setup_nvm.sh

--- cobbler/steps/setup_nvm.sh
+++ cobbler/steps/setup_nvm.sh
@@ -4,9 +4,9 @@ set -e;
 echo "Running NVM installer";
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash;
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo "Setting up NVM";
 /bin/bash -c 'nvm install $1; nvm use $1; nvm alias default $1;'
