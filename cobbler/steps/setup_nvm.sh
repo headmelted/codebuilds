0dfef590ac2b86577b8429988db9c760acda4c9b
Jay Rodgers <headmelted@gmail.com>
2018-09-20 18:32:25 +0100

    Update setup_nvm.sh

--- cobbler/steps/setup_nvm.sh
+++ cobbler/steps/setup_nvm.sh
@@ -9,9 +9,7 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 echo "Setting up NVM";
-nvm install $1;
-nvm use $1;
-nvm alias default $1;
+/bin/bash -c 'nvm install $1; nvm use $1; nvm alias default $1;'
   
 echo "Setting python binding";
 npm config set python `which python`;
