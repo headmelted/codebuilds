e5f4ed236154eda31c6a2cd13ee8738d7a5c7e0e
Jay Rodgers <headmelted@gmail.com>
2018-09-20 14:30:28 +0100

    Update dock.sh

--- dock.sh
+++ dock.sh
@@ -8,7 +8,7 @@
 #
 # usage: dock.sh <docker_hub_image> <bash_script_to_run_in_container>
 
-docker_image="headmelted/cobbler:$COBBLER_ARCH"
+docker_image="headmelted/cobbler:$COBBLER_STRATEGY-$COBBLER_ARCH"
 
 echo "Checking if $docker_image exists locally"
 if [[ "$(docker images -q $docker_image 2> /dev/null)" != "" ]]; then
@@ -36,4 +36,4 @@ docker run -it --security-opt apparmor:unconfined --cap-add SYS_ADMIN \
 -e COBBLER_GIT_ENDPOINT=$COBBLER_GIT_ENDPOINT \
 -v $(pwd)/cobbler:/root/kitchen/cobbler \
 -v $(pwd)/output:/root/kitchen/output \
-headmelted/cobbler:$COBBLER_STRATEGY-$COBBLER_ARCH;
+$docker_image;
