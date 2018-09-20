25340150ca83bfb3cea8a415fef8e8dd5eef59c2
Jay Rodgers <headmelted@gmail.com>
2018-09-20 18:18:58 +0100

    Update setup_nvm.sh

--- cobbler/steps/setup_nvm.sh
+++ cobbler/steps/setup_nvm.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 set -e;
 
-echo "Removing old nvm for user if it exists";
-rm -rf ~/.nvm;
-
-echo "Cloning nvm for user";
-git clone --depth 1 https://github.com/creationix/nvm.git ~/.nvm;
-
-echo "Setting current owner as owner of ~/.nvm";
-chown ${USER:=$(/usr/bin/id -run)}:$USER -R ~/.nvm;
-
-echo "NVM Script:"
-cat ~/.nvm/nvm.sh;
-
-echo "Running NVM script";
-. ~/.nvm/nvm.sh;
-
 echo "Setting up NVM";
 nvm install $1;
 nvm use $1;
