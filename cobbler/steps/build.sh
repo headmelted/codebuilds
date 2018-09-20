342330aa92c8c3af4235a862d963330fa6ac2bc5
Jay Rodgers <headmelted@gmail.com>
2018-09-20 18:18:18 +0100

    Update build.sh

--- cobbler/steps/build.sh
+++ cobbler/steps/build.sh
@@ -5,8 +5,7 @@ cd $COBBLER_CODE_DIRECTORY;
 
 chmod +x ~/cobbler/steps/*.sh;
 
-echo "Running NVM installer";
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+~/cobbler/steps/setup_nvm.sh;
 
 echo "Running npm install for $npm_config_target_arch";
 npm install --target-arch=$npm_config_target_arch --unsafe-perm;
