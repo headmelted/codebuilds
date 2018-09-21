982b35e1b59393902df0575f69de1355444ce91e
Jay Rodgers <headmelted@gmail.com>
2018-09-21 10:55:58 +0100

    Update build.sh

--- cobbler/steps/build.sh
+++ cobbler/steps/build.sh
@@ -24,3 +24,15 @@ yarn install;
 
 echo "Compiling VS Code for $npm_config_target_arch";
 yarn run gulp vscode-linux-$npm_config_target_arch-min;
+
+echo "Starting vscode-linux-$npm_config_target_arch-build-deb";
+yarn run gulp vscode-linux-$npm_config_target_arch-build-deb;
+
+echo "Moving deb packages for release";
+mv $COBBLER_CODE_DIRECTORY/.build/linux/deb/$COBBLER_ARCH/deb/*.deb $COBBLER_OUTPUT_DIRECTORY;
+
+echo "Starting vscode-linux-$npm_config_target_arch-build-rpm";
+yarn run gulp "vscode-linux-$npm_config_target_arch-build-rpm";
+
+echo "Moving rpm packages for release";
+mv $COBBLER_CODE_DIRECTORY/.build/linux/rpm/$COBBLER_RPM_ARCH/rpmbuild/RPMS/$COBBLER_RPM_ARCH/*.rpm $COBBLER_OUTPUT_DIRECTORY;
