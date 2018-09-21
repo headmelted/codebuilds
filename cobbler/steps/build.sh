c2cc7ba0f3a19826066932d80206b8b5a20f3660
Jay Rodgers <headmelted@gmail.com>
2018-09-21 06:43:38 +0100

    Update build.sh

--- cobbler/steps/build.sh
+++ cobbler/steps/build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 set -e;
 
-cd $COBBLER_CODE_DIRECTORY;
-
-chmod +x ~/cobbler/steps/*.sh;
-
-~/cobbler/steps/setup_nvm.sh 8.9.4;
+. ~/cobbler/steps/setup_nvm.sh;
 
 echo "Running npm install for $npm_config_target_arch";
-npm install --target-arch=$npm_config_target_arch --unsafe-perm;
+yarn install;
 
 echo "Compiling VS Code for $npm_config_target_arch";
 yarn run gulp vscode-linux-$npm_config_target_arch-min;
