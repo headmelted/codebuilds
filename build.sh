e3c5ad357a1a5f0d3f038a63608cf39bd575d02b
Jay Rodgers <headmelted@gmail.com>
2018-10-10 15:20:34 +0000

    Debugging yarn failures

--- build.sh
+++ build.sh
@@ -24,8 +24,8 @@ echo "Installing NVM and dependencies";
 echo "Entering code directory";
 cd code;
 
-echo "Running npm install";
-npm install;
+echo "Running yarn install";
+yarn;
 
 echo "Compiling VS Code for $npm_config_arch";
 yarn run gulp vscode-linux-$npm_config_arch-min;
