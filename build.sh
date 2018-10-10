7c43174a76afe2001a166e62c2710ae90726fac4
Jay Rodgers <headmelted@gmail.com>
2018-10-10 13:39:27 +0000

    latest updates

--- build.sh
+++ build.sh
@@ -3,14 +3,14 @@ set -e;
 
 vscode_git="https://github.com/Microsoft/vscode.git"
 
-echo "Retrieving code from git endpoint [$vscode_git] into [$COBBLER_CODE_DIRECTORY]";
-git clone $vscode_git $COBBLER_CODE_DIRECTORY;
+echo "Retrieving code from git endpoint [$vscode_git] into [code]";
+git clone $vscode_git code;
   
 echo "Setting current owner as owner of code folder";
-chown ${USER:=$(/usr/bin/id -run)}:$USER -R $COBBLER_CODE_DIRECTORY;
+chown ${USER:=$(/usr/bin/id -run)}:$USER -R code;
 
 echo "Synchronizing overlays folder";
-rsync -avh ./overlays/ $COBBLER_CODE_DIRECTORY/;
+rsync -avh ./overlays/ code/;
 
 echo "Running yarn install";
 yarn install;
@@ -22,25 +22,25 @@ echo "Starting vscode-linux-$npm_config_arch-build-deb";
 yarn run gulp vscode-linux-$npm_config_arch-build-deb;
 
 echo "Moving deb packages for release";
-mv $COBBLER_CODE_DIRECTORY/.build/linux/deb/$COBBLER_ARCH/deb/*.deb $COBBLER_OUTPUT_DIRECTORY;
+mv ./code/.build/linux/deb/$COBBLER_ARCH/deb/*.deb output;
 
 echo "Extracting deb archive";
-dpkg -x $COBBLER_OUTPUT_DIRECTORY/*.deb $COBBLER_OUTPUT_DIRECTORY/extracted;
+dpkg -x output/*.deb output/extracted;
 
 #echo "Reading ELF dependencies";
-#readelf -d $COBBLER_OUTPUT_DIRECTORY/extracted/usr/share/code-oss/code-oss;
+#readelf -d output/extracted/usr/share/code-oss/code-oss;
 
-cd $COBBLER_OUTPUT_DIRECTORY/extracted;
+cd output/extracted;
 echo "Binary components of output --------------------------------------------------"
 find . -type f -exec file {} ";" | grep ELF
 echo "------------------------------------------------------------------------------"
 
 echo "Dependency tree for code-oss -------------------------------------------------"
-ldd -v $COBBLER_OUTPUT_DIRECTORY/extracted/usr/share/code-oss/code-oss;
+ldd -v output/extracted/usr/share/code-oss/code-oss;
 echo "------------------------------------------------------------------------------"
 
 #echo "Starting vscode-linux-$npm_config_target_arch-build-rpm";
 #yarn run gulp "vscode-linux-$npm_config_target_arch-build-rpm";
 
 #echo "Moving rpm packages for release";
-#mv $COBBLER_CODE_DIRECTORY/.build/linux/rpm/$COBBLER_RPM_ARCH/rpmbuild/RPMS/$COBBLER_RPM_ARCH/*.rpm $COBBLER_OUTPUT_DIRECTORY;
+#mv $COBBLER_CODE_DIRECTORY/.build/linux/rpm/$COBBLER_RPM_ARCH/rpmbuild/RPMS/$COBBLER_RPM_ARCH/*.rpm output;
