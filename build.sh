82a25abb253a1fbe724359260e82cd761a9be642
Jay Rodgers <headmelted@gmail.com>
2019-02-12 15:54:19 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ set -e;
 echo "pkg-config search path --------------------------";
 pkg-config --variable pc_path pkg-config;
 
+echo "Emulators available:";
+update-binfmts --display;
+
 . /root/kitchen/tools/archie_jail.sh ". /root/kitchen/tools/setup_nvm.sh";
 
 echo "Current directory is:";
