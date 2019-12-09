99b3599e91bbeb9fd08ef78c19a206e77d18e28d
Jay Rodgers <headmelted@gmail.com>
2019-12-09 14:44:26 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -59,8 +59,8 @@ mkdir output;
 echo "Moving deb packages for release";
 mv ./code/.build/linux/deb/$ARCHIE_ARCH/deb/*.deb /root/output;
 
-echo "Moving rpm packages for release";
-mv ./code/.build/linux/rpm/$ARCHIE_RPM_ARCH/rpmbuild/RPMS/$ARCHIE_RPM_ARCH/*.rpm /root/output;
+#echo "Moving rpm packages for release";
+#mv ./code/.build/linux/rpm/$ARCHIE_RPM_ARCH/rpmbuild/RPMS/$ARCHIE_RPM_ARCH/*.rpm /root/output;
 
 echo "Extracting deb archive";
 dpkg -x /root/output/*.deb output/extracted;
